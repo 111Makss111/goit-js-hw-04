aa02861d467ae8430cd37e1c2e67809318632166
111Makss111 <151084313+111Makss111@users.noreply.github.com>
2024-04-17 20:17:51 +0200

    return

--- js/task-1.js
+++ js/task-1.js
@@ -3,7 +3,7 @@ function isEnoughCapacity(products, containerSize) {
   for (const key in products) {
     total += products[key];
   }
-  return total < containerSize;
+  return total <= containerSize;
 }
 
 console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)); // true
